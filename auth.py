34e2a92f82bb3bd9b361ab6c39b6281a0d6dca1a
Ranjithkumarboya <120434843+Ranjithkumarboya@users.noreply.github.com>
2026-08-01 22:13:24 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,6 +1,7 @@
 from kiteconnect import KiteConnect
 import streamlit as st
 
+
 class ZerodhaAuth:
 
     def __init__(self):
@@ -11,11 +12,11 @@ class ZerodhaAuth:
 
     def generate_session(self, request_token):
 
-    data = self.kite.generate_session(
-        request_token,
-        api_secret=st.secrets["API_SECRET"]
-    )
+        data = self.kite.generate_session(
+            request_token,
+            api_secret=st.secrets["API_SECRET"]
+        )
 
-    self.kite.set_access_token(data["access_token"])
+        self.kite.set_access_token(data["access_token"])
 
-    return data["access_token"]
+        return data["access_token"]

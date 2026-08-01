519129f80f82b907221113e13a53a82c040057c3
Ranjithkumarboya <120434843+Ranjithkumarboya@users.noreply.github.com>
2026-08-01 15:50:58 +0530

    Create option_chain.py

---
+++ option_chain.py
@@ -0,0 +1,22 @@
+"""
+ALPHA v2
+Option Chain Engine
+"""
+
+class OptionChain:
+
+    def analyze(self):
+
+        return {
+
+            "PCR": None,
+
+            "Max Pain": None,
+
+            "Call OI": None,
+
+            "Put OI": None,
+
+            "Decision": "Coming Soon"
+
+        }

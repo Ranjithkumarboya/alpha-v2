c70363ae32afe01ce04fb6ca50e33162dab1f70e
Ranjithkumarboya <120434843+Ranjithkumarboya@users.noreply.github.com>
2026-08-01 16:03:32 +0530

    Create market_data.py

---
+++ market_data.py
@@ -0,0 +1,36 @@
+from kiteconnect import KiteConnect
+import streamlit as st
+
+
+class MarketData:
+
+    def __init__(self):
+        self.kite = KiteConnect(api_key=st.secrets["API_KEY"])
+
+        if "ACCESS_TOKEN" in st.session_state:
+            self.kite.set_access_token(st.session_state["ACCESS_TOKEN"])
+
+    def quote(self, symbol):
+        try:
+            return self.kite.quote([symbol])[symbol]
+        except Exception:
+            return None
+
+    def ltp(self, symbol):
+        try:
+            return self.kite.ltp([symbol])[symbol]["last_price"]
+        except Exception:
+            return None
+
+    def ohlc(self, symbol):
+        try:
+            return self.kite.quote([symbol])[symbol]["ohlc"]
+        except Exception:
+            return None
+
+    def market_status(self):
+        try:
+            quote = self.kite.quote(["NSE:NIFTY 50"])
+            return "OPEN"
+        except Exception:
+            return "CLOSED"

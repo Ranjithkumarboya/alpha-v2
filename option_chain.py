6b4439b3f4928d186a737aea07da525968bd7cbe
Ranjithkumarboya <120434843+Ranjithkumarboya@users.noreply.github.com>
2026-08-02 12:37:04 +0530

    Update option_chain.py

--- option_chain.py
+++ option_chain.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from market_data import MarketData
 
 
@@ -8,13 +9,31 @@ class OptionChain:
 
     def atm_strike(self):
         nifty = self.market.ltp("NSE:NIFTY 50")
+
+        if nifty is None:
+            return None
+
         return round(nifty / 50) * 50
 
+    def current_expiry(self):
+        today = datetime.today()
+
+        year = str(today.year)[2:]
+        month = today.strftime("%b").upper()
+
+        return f"{year}{month}"
+
     def option_symbols(self):
+
         strike = self.atm_strike()
 
-        ce = f"NFO:NIFTYAUTO{strike}CE"
-        pe = f"NFO:NIFTYAUTO{strike}PE"
+        if strike is None:
+            return None
+
+        expiry = self.current_expiry()
+
+        ce = f"NFO:NIFTY{expiry}{strike}CE"
+        pe = f"NFO:NIFTY{expiry}{strike}PE"
 
         return {
             "CE": ce,
@@ -22,12 +41,19 @@ class OptionChain:
         }
 
     def option_prices(self):
+
         symbols = self.option_symbols()
 
-        ce_price = self.market.ltp(symbols["CE"])
-        pe_price = self.market.ltp(symbols["PE"])
+        if symbols is None:
+            return {
+                "CE": None,
+                "PE": None
+            }
+
+        ce = self.market.ltp(symbols["CE"])
+        pe = self.market.ltp(symbols["PE"])
 
         return {
-            "CE": ce_price,
-            "PE": pe_price
+            "CE": ce,
+            "PE": pe
         }

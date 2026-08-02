ecc08f7b095d73ccb3d1e8d5c1d265c9eb8651b7
Ranjithkumarboya <120434843+Ranjithkumarboya@users.noreply.github.com>
2026-08-02 09:03:06 +0530

    Update option_chain.py

--- option_chain.py
+++ option_chain.py
@@ -1,22 +1,42 @@
-"""
-ALPHA v2
-Option Chain Engine
-"""
+from kiteconnect import KiteConnect
+import streamlit as st
+
 
 class OptionChain:
 
-    def analyze(self):
+    def __init__(self):
+        self.kite = KiteConnect(api_key=st.secrets["API_KEY"])
+
+        if "access_token" in st.session_state:
+            self.kite.set_access_token(
+                st.session_state["access_token"]
+            )
+
+    def ltp(self):
+        try:
+            return self.kite.ltp(["NSE:NIFTY 50"])["NSE:NIFTY 50"]["last_price"]
+        except Exception:
+            return None
+
+    def atm_strike(self):
+
+        ltp = self.ltp()
+
+        if ltp is None:
+            return None
+
+        return round(ltp / 50) * 50
+
+    def current_expiry(self):
+        return "AUTO"
+
+    def option_symbols(self):
+
+        strike = self.atm_strike()
+
+        expiry = self.current_expiry()
 
         return {
-
-            "PCR": None,
-
-            "Max Pain": None,
-
-            "Call OI": None,
-
-            "Put OI": None,
-
-            "Decision": "Coming Soon"
-
+            "CE": f"NFO:NIFTY{expiry}{strike}CE",
+            "PE": f"NFO:NIFTY{expiry}{strike}PE"
         }
